6fa827eaa268f539c2cbe8357e936a85b6e9b263
Anurag Singh <aaa.singh905@gmail.com>
2021-01-19 19:09:49 +0530

    SignUp Form

---
+++ components/spacer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const spacer = ({ children }) => {
+    return (
+        <View style={styles.spacer}>
+           {children}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    spacer:{
+        margin:15
+    }
+});
+
+export default spacer
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,16 +1,37 @@
+import React from "react";
+import { View, TouchableOpacity,StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/spacer";
 
-import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+const SignupScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" onPress={() => navigation.navigate("Signin")} />
+      </Spacer>
+      <TouchableOpacity onPress={() => navigation.navigate("Signin")}><Text>Already Have an account?</Text></TouchableOpacity>
+    </View>
+  );
+};
 
-const SignupScreen = ({navigation}) => {
-    return (
-        <View>
-            <Text style={{fontSize:48}}>SignUp Screen</Text>
-            <Button title="Go to SignIn" onPress={()=> navigation.navigate('Signin')}/>
-            <Button title="Go to Main Flow" onPress={()=> navigation.navigate('mainFlow')}/>
-        </View>
-    )
-}
+SignupScreen.navigationOptions = () => {
+    return {
+      headerShown: false,
+    };
+  };
 
-const styles = StyleSheet.create({})
-export default SignupScreen
+const styles = StyleSheet.create({
+  container: {
+      flex:1,
+      justifyContent:"center",
+      alignItems:'center',
+      marginBottom:'40%'
+  },
+});
+export default SignupScreen;
